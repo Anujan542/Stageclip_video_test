8a70e94fd26c6326443fd37febf726cfa55d5747
anujan542 <anujan9300@gmail.com>
2024-06-05 16:38:40 +0530

    Added intro and outro

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import { Player } from "@remotion/player";
-import React, { useEffect, useState } from "react";
 import PlayerCheck from "./components/Player";
-import { getVideoMetadata } from "@remotion/media-utils";
+import { AbsoluteFill } from "remotion";
+import { useCallback } from "react";
+import { LoaderIcon } from "lucide-react";
 
 const App = () => {
-  const [startTime, setStartTime] = useState("");
-  const [endTime, setEndTime] = useState("");
-  const [loading, setLoading] = useState(true);
+  const renderPoster = useCallback(({ isBuffering }) => {
+    if (isBuffering) {
+      return (
+        <AbsoluteFill
+          style={{ justifyContent: "center", alignItems: "center" }}
+        >
+          <LoaderIcon className="animate-spin" />
+          Please wait 5-10 sec your ceremony video in progress 😎🥳😍
+        </AbsoluteFill>
+      );
+    }
 
-  const [totalDuration, setTotalDuration] = useState(1);
-
-  const [startTimeFrame, setStartTimeFrame] = useState(0);
-  const [endTimeFrame, setEndTimeFrame] = useState(1);
-
-  const calculateDuration = async () => {
-    setLoading(true);
-    const { durationInSeconds } = await getVideoMetadata(
-      "https://clippingplatformprod.blob.core.windows.net/assets/clients/000c8f8a-e849-4bbe-af06-b620ddc193e9/events/e91058cf-ebd7-4b16-892e-1d5fd13c5e48/SJU_2024_Graduate_Level_Commencement_PM_Ceremony-5948.mp4?st=2024-06-04T15%3A22%3A36Z&se=2024-06-05T15%3A22%3A36Z&sp=r&sv=2018-03-28&sr=b&sig=j%2BHjqKV13WrmY3D%2FfMgfwoaauSlb5v5u3qNtXwAbki0%3D"
-    );
-    setTotalDuration(Math.round(durationInSeconds * 30));
-    setEndTimeFrame(Math.round(durationInSeconds * 30));
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    calculateDuration();
+    return null;
   }, []);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setStartTimeFrame(startTime * 60 * 30);
-    setEndTimeFrame(endTime * 60 * 30);
-  };
-
   return (
     <>
       <div className="flex justify-center items-center mt-10">
@@ -40,63 +26,22 @@ const App = () => {
           Stageclip Ceremony Video Testing
         </h1>
       </div>
-      <div className="flex justify-center items-center gap-6 mt-6 mb-6">
-        <input
-          type="number"
-          className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          placeholder="enter start time"
-          onChange={(e) => setStartTime(e.target.value)}
-        />
-        <input
-          type="number"
-          className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          placeholder="enter start time"
-          onChange={(e) => setEndTime(e.target.value)}
-        />
 
-        {startTime === "" ||
-        endTime === "" ||
-        Number(endTime) < Number(startTime) ? (
-          <button
-            className="bg-gray-300 px-4 py-2 rounded-md opacity-50"
-            disabled
-          >
-            Submit
-          </button>
-        ) : (
-          <button
-            onClick={handleSubmit}
-            className="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
-          >
-            Submit
-          </button>
-        )}
-      </div>
       <div className="flex justify-center items-center gap-6 mt-10">
-        {loading ? (
-          <h1 className="text-gray-800">
-            Please wait your ceremony video is Loading....
-          </h1>
-        ) : (
-          <Player
-            component={PlayerCheck}
-            durationInFrames={Number(totalDuration)}
-            compositionWidth={1920}
-            compositionHeight={1080}
-            fps={30}
-            controls={true}
-            style={{
-              width: 800,
-              height: 450,
-            }}
-            inFrame={Number(startTimeFrame)}
-            outFrame={
-              Number(endTimeFrame) === Number(totalDuration)
-                ? Number(totalDuration) - 1
-                : Number(endTimeFrame)
-            }
-          />
-        )}
+        <Player
+          component={PlayerCheck}
+          durationInFrames={910}
+          compositionWidth={1920}
+          compositionHeight={1080}
+          fps={30}
+          controls={true}
+          style={{
+            width: 800,
+            height: 450,
+          }}
+          renderPoster={renderPoster}
+          showPosterWhenBuffering
+        />
       </div>
     </>
   );
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,26 +1,43 @@
-import React from "react";
-import { Video } from "remotion";
-import { preloadVideo, resolveRedirect } from "@remotion/preload";
+import React, { useEffect } from "react";
+import { AbsoluteFill, Sequence, Video } from "remotion";
+import { preloadVideo } from "@remotion/preload";
 
-let urlToLoad =
+let intro =
+  "https://remotionlambda-jfkbnf6jjh.s3.eu-west-1.amazonaws.com/cache/f8de3a85-ee1b-4e91-ba41-c3435446b5c3/825cca22-cf0d-40de-85c4-d8dcdafcc522.mp4";
+let outro =
+  "https://remotionlambda-jfkbnf6jjh.s3.eu-west-1.amazonaws.com/cache/f8de3a85-ee1b-4e91-ba41-c3435446b5c3/b4fcdd47-2468-4513-b632-71d75e45c547.mp4";
+let ceremonyVideo =
   "https://clippingplatformprod.blob.core.windows.net/assets/clients/000c8f8a-e849-4bbe-af06-b620ddc193e9/events/e91058cf-ebd7-4b16-892e-1d5fd13c5e48/SJU_2024_Graduate_Level_Commencement_PM_Ceremony-5948.mp4?st=2024-06-04T15%3A22%3A36Z&se=2024-06-05T15%3A22%3A36Z&sp=r&sv=2018-03-28&sr=b&sig=j%2BHjqKV13WrmY3D%2FfMgfwoaauSlb5v5u3qNtXwAbki0%3D";
 
-resolveRedirect(urlToLoad)
-  .then((resolved) => {
-    urlToLoad = resolved;
-  })
-  .catch((err) => {
-    console.log("Could not resolve redirect", err);
-  })
-  .finally(() => {
-    preloadVideo(urlToLoad);
-  });
-
 const PlayerCheck = () => {
+  useEffect(() => {
+    const unpreloadIntroVideo = preloadVideo(intro);
+    const unpreloadOutroVideo = preloadVideo(outro);
+    const unpreloadVideo = preloadVideo(ceremonyVideo);
+
+    unpreloadIntroVideo();
+    unpreloadOutroVideo();
+    unpreloadVideo();
+  }, []);
+
   return (
-    <div>
-      <Video src={urlToLoad} />
-    </div>
+    <AbsoluteFill>
+      <Sequence from={0} durationInFrames={421}>
+        <Video pauseWhenBuffering src={intro} />
+      </Sequence>
+      <Sequence premountFor={0} from={421} durationInFrames={661}>
+        <Video
+          pauseWhenBuffering
+          startFrom={113400}
+          endAt={113640}
+          src={ceremonyVideo}
+          muted
+        />
+      </Sequence>
+      <Sequence from={660} durationInFrames={910}>
+        <Video pauseWhenBuffering src={outro} />
+      </Sequence>
+    </AbsoluteFill>
   );
 };
 
